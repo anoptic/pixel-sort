1019af641effdce57e8b083eb948d41efc4f4325
anoptic <50808486+anoptic@users.noreply.github.com>
2022-07-09 23:37:56 +0200

    displaing images without type errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,26 @@ import Controls from "./components/controls/Controls";
 import Header from "./components/Header";
 import Image from "./components/Image";
 
+interface initImage {
+  desktop: string,
+  mobile: string
+}
+
+const initImage: initImage = {
+  desktop: "/assets/heads.jpg", 
+  mobile: "/assets/inkblot.jpg"
+}
+
 const App = () => {
-  const [imageData, setImageData] = useState(null);
+  const [rawImage, setRawImage] = useState<string>(initImage.desktop);
+  const [imageData, setImageData] = useState<ImageData | null>(null);
+
+  // if (imageData) console.log(imageData);
 
   return (
     <>
       <Header />
-      <Image setImageData={setImageData} />
+      <Image rawImage={rawImage} setImageData={setImageData} />
       <Controls />
     </>
   )
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,34 +1,26 @@
 import { useEffect, useRef, useState } from "react";
 
-interface Callback {
-  setImageData: (...args: any[]) => void
+interface ImageProps {
+  rawImage: string,
+  setImageData: (imageData: ImageData) => void
 }
 
-interface initImage {
-  desktop: string,
-  mobile: string
-}
+const displayImage = new window.Image();
 
-const initImage: initImage = {
-  desktop: "/assets/heads.jpg", 
-  mobile: "/assets/inkblot.jpg"
-}
-
-const Image = ({setImageData}: Callback) => { 
-  const [rawImage, setRawImage] = useState(initImage.desktop);
-  const canvasRef = useRef(null);
+const Image = ({rawImage, setImageData}: ImageProps) => { 
+  const canvasRef = useRef<HTMLCanvasElement>(null!);
 
   useEffect(() => {
-    const canvas: any = canvasRef.current;
+    const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
 
-    const displayImage = new window.Image();
+    // const displayImage = new window.Image();
     displayImage.src = rawImage;
     displayImage.onload = () => {
       canvas.width = displayImage.naturalWidth;
       canvas.height = displayImage.naturalHeight;
-      context.drawImage(displayImage, 0, 0);
-      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      context!.drawImage(displayImage, 0, 0);
+      const imageData = context!.getImageData(0, 0, canvas.width, canvas.height);
       setImageData(imageData);
       // console.log(imageData);
     }
